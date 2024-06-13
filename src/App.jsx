2f1317724303bf4ae1d6a7e833aad778e7344aba
Bruno Canuto <bruno.canuto@toxicologiapardini.com.br>
2024-06-13 19:23:35 -0300

    Ajuste menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,36 @@
+import { useState } from 'react';
 import './App.css'
 import videoDrRafu  from './assets/videos/Dr-Rafu-Jr-fdown.net.mp4'
 
 export default function App() {
 
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   const telefone = '(11) 9 9696-5481'
   const email = 'rafu.jr@hotmail.com'
   const naturalidade = 'Suzano - São Paulo'
 
+  console.log(isSidebarOpen)
+
   return (
     <div>
       
-      <button id="sidebarToggle" data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+      <button onClick={toggleSidebar} data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar" type="button" className="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
         <span className="sr-only">Menu</span>
         <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
         <path clip-rule="evenodd" fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
         </svg>
       </button>
 
-      <aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 md:w-64 lg:w-96 h-screen transition-transform -translate-x-full sm:translate-x-0 border-r-2 border-gray-100" aria-label="Sidebar">
+      <aside id="default-sidebar" className={`fixed top-0 left-0 z-40 w-64 md:w-64 lg:w-96 h-screen transition-transform ${!isSidebarOpen ? 'md:-translate-x-0 -translate-x-full' : ' ' } border-r-2 border-gray-100`} aria-label="Sidebar">
         <div className="h-full pt-20 pb-20 overflow-y-auto bg-white dark:bg-gray-800 flex flex-col items-center justify-between">
             
+            <a href="javascript:void(0)" className='text-xl absolute top-4 right-4 md:hidden lg:hidden' onClick={toggleSidebar}>x</a>
+
             <div className='flex flex-col items-center'>
               <img className="w-28 h-28 rounded-full ring-2 ring-violet-400 dark:ring-gray-500" src="https://cdn.jornaldebrasilia.com.br/wp-content/uploads/2022/05/18185728/1-1-scaled.jpg" alt="Bordered avatar" />
               
